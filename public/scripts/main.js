cd1a9e2a589d707683688133e47edfdd2b8ce933
Jessica Del Grande <jessica.delgrande@gmail.com>
2018-10-20 17:06:12 -0400

    built out separate pages, added bg colours

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,29 @@
 "use strict";
 
+// NAV HOVERS
+$("ul li").hover(function () {
+	$(this).siblings().stop().fadeTo(200, 0.3);
+}, function () {
+	$(this).siblings().stop().fadeTo(400, 1);
+});
+
+// NAVBAR SHRINK ON SCROLL
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+// 	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+// 		document.getElementById("navContainer").style.padding = "16px 48px 8px 0";
+// 		document.getElementById("navRobot").style.width = "24px";
+// 		document.getElementById("navRobot").style.height = "24px";
+// 		document.getElementById("navRobot").style.backgroundSize = "24px";
+// 	} else {
+// 		document.getElementById("navContainer").style.padding = "24px 48px 16px 0";
+// 		document.getElementById("navRobot").style.width = "48px";
+// 		document.getElementById("navRobot").style.height = "48px";
+// 		document.getElementById("navRobot").style.backgroundSize = "48px";
+// 	}
+// }
+
 // WHAT WE PROVIDE - SLIDE-DOWN TEXT BLOCKS
 $(".provideText-lunch").click(function () {
 	$(".provideHidden-lunch").slideToggle();
@@ -26,8 +50,8 @@ $(".sponsorText-enactus").click(function () {
 	$(".sponsorHidden-enactus").slideToggle();
 });
 
-$(".sponsorText-wework").click(function () {
-	$(".sponsorHidden-wework").slideToggle();
+$(".sponsorText-communihelp").click(function () {
+	$(".sponsorHidden-communihelp").slideToggle();
 });
 
 $(".sponsorText-tchc").click(function () {
